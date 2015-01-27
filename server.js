6455c5c9662692dc6cff1e30d24fffb49dcd9063
Juha Paananen <juha.paananen@gmail.com>
2015-01-27 20:01:55 +0200

    migrate to cedar-14/express

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express')
+var port = process.env.PORT || 3000
+var app = express()
+
+app.use(express.compress())
+app.use('/', express.static(__dirname + "/public"))
+app.listen(port)
